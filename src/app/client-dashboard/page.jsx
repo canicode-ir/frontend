2275e809c1fd396c2b459a302b402ac36dd892a9
Saeid JaliliBashashi <saeidemoun@gmail.com>
2024-09-07 17:54:19 +0430

    LogOut Button Handler is Temporarily set in client dashborad route

--- src/app/client-dashboard/page.jsx
+++ src/app/client-dashboard/page.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+"use client";
+
+import axios from "axios";
+import { BASE_URL } from "../../services/api";
+import Cookies from "js-cookie";
 
 function page() {
-  return <div>User dashboard</div>;
+  const logOutHandler = async (e) => {
+    e.preventDefault();
+    const token = Cookies.get("token");
+    try {
+      await axios.put(
+        `${BASE_URL}user/logout`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      Cookies.remove("token");
+      window.location.href = "/";
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  return (
+    <div>
+      <button
+        className="w-fit p-2 bg-red400 font-bold text-sm text-white rounded-sm"
+        onClick={logOutHandler}
+      >
+        خروج از حساب کاربری
+      </button>
+    </div>
+  );
 }
 
 export default page;
