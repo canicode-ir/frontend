8c73e946f9ecb089e910a7380817699c81a6826f
Saeid JaliliBashashi <saeidemoun@gmail.com>
2024-09-12 15:01:42 +0430

    PaymentStatus Components are beign established

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,5 +1,13 @@
-function page() {
-  return <div>page</div>;
+//Components
+import PaymentStatus from "../../components/templates/paymentStatus/PaymentStatus";
+
+function page({ searchParams }) {
+  console.log(searchParams);
+  return (
+    <div>
+      <PaymentStatus />
+    </div>
+  );
 }
 
 export default page;
---
+++ src/components/templates/paymentStatus/PaymentStatus.jsx
@@ -0,0 +1,5 @@
+function PaymentStatus() {
+  return <div>PaymentStatus</div>;
+}
+
+export default PaymentStatus;
